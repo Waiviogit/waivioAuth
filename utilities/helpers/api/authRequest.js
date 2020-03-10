84504f6eb9dc882a32e63921fd7a1aafc35cccf7
olegtsigulov <olegcigulev@gmail.com>
2020-03-10 11:08:09 +0200

    create users on both servers

--- utilities/helpers/api/authRequest.js
+++ utilities/helpers/api/authRequest.js
@@ -3,7 +3,7 @@ const config = require( '../../../config' );
 
 const signUpRequest = async ( data ) => {
     try {
-        await axios.post( `${config.authUrl}auth/create_user`, data, { headers: { 'API_KEY': process.env.API_KEY } } );
+        await axios.post( `${config.authUrl}auth/create_user`, data, { headers: { 'api-key': process.env.API_KEY } } );
     }catch( error ) {
         console.error( error.message );
     }
