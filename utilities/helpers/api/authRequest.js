afef2b01802d41a7b01a25fc89462919260d3510
olegtsigulov <olegcigulev@gmail.com>
2020-03-13 15:56:03 +0200

    add check for _id

--- utilities/helpers/api/authRequest.js
+++ utilities/helpers/api/authRequest.js
@@ -3,7 +3,7 @@ const config = require( '../../../config' );
 
 const signUpRequest = async ( data ) => {
     try {
-        data.avatar ? data.avatar : '';
+        data.avatar = data.avatar ? data.avatar : '';
         await axios.post( `${config.authUrl}auth/create_user`, data, { headers: { 'api-key': process.env.API_KEY } } );
     }catch( error ) {
         console.error( error.message );
