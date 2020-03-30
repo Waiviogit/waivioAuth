0bccbdb584a1bd728376106776220edec0719030
olegtsigulov <olegcigulev@gmail.com>
2020-03-30 10:12:30 +0300

    default nightMode with right headers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require( 'cors' );
 const app = express();
 
 global._ = require( 'lodash' );
-app.use( cors( { exposedHeaders: [ 'access-token', 'expires-in', 'waivio-auth', 'um_session', 'api-key' ] } ) );
+app.use( cors( { exposedHeaders: [ 'access-token', 'expires-in', 'waivio-auth', 'um_session', 'api-key', 'nightmode' ] } ) );
 require( './enviroment.js' )( app, express );
 require( './auth.js' )( app );
 const routes = require( './routes' );
