00d2bc82d471156309a548de8a159129207aba0a
olegtsigulov <olegcigulev@gmail.com>
2020-04-15 14:29:52 +0300

    change view

--- views/authenticationController/validateAuthToken.js
+++ views/authenticationController/validateAuthToken.js
@@ -1,5 +1,5 @@
 module.exports = function( data ) {
     return {
-        user: _.omit( data.user, [ 'auth.sessions', 'auth.id' ] )
+        user: Object.assign( _.omit( data.user.auth, [ 'id', 'sessions' ] ), _.omit( data.user, [ 'auth' ] ) )
     };
 };
