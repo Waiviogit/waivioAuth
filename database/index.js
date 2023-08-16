5ac666b8dd16835b39a9729538955a0c80dc08f1
fesmofet <borees2@gmail.com>
2023-08-16 15:28:40 +0300

    connection_string

--- database/index.js
+++ database/index.js
@@ -1,6 +1,10 @@
 const config = require( '../config' );
 const mongoose = require( 'mongoose' );
-const URI = `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`;
+
+
+const URI = process.env.MONGO_URI_WAIVIO
+    ? process.env.MONGO_URI_WAIVIO
+    : `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`;
 
 mongoose.connect( URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true } )
     .then( () => console.log( 'connection successful!' ) )
